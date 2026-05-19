d6c44ee8e1ae6a7892e359ede5b63b5b9defd6b3
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2026-05-20 01:23:04 +0530

    add chromadb connection

---
+++ src/db/chroma.ts
@@ -0,0 +1,23 @@
+import { CloudClient, type Collection } from "chromadb";
+
+const client = new CloudClient({
+    apiKey: process.env.CHROMA_API_KEY!,
+    tenant: process.env.CHROMA_TENANT!,
+    database: process.env.CHROMA_DATABASE!,
+});
+
+let insuranceQACollection: Collection | null = null;
+
+export const getInsuranceQACollection = async () => {
+	if (!insuranceQACollection) {
+		insuranceQACollection = await client.getOrCreateCollection({
+			name: "insurance_qa_collection",
+		});
+	}
+	return insuranceQACollection;
+};
+
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import { connectDB, db, insertSampleData, resetDatabase } from "./db/db.js";
-import fs from "fs";
+import "dotenv/config";
+import { connectDB, db, insertSampleData } from "./db/sqlite.js";
+import { processInsuranceDatasetToChroma } from "./utils/processInsuranceDatasetToChroma.js";
+import { getInsuranceQACollection } from "./db/chroma.js";
 
+// Connect to SQLite database
 connectDB();
+
+// Insert sample data(fake) into SQLite DB, make sure run it once
 // insertSampleData();
 
+// Test SQLite
 const stmt = db.prepare("SELECT * from customers LIMIT 10");
+if (stmt.columns().length > 10) {
+	console.log("DB Connection Successfull.");
+}
 
-stmt.all().forEach((row) => {
-	// fs.writeFileSync("./output.txt", JSON.stringify(row, null, 2), { flag: "a+" });	
-	// console.log(JSON.stringify(row, null, 2));
-	console.log(JSON.stringify(row, null, 2));
-	
-});
+// fetch insurance QA dataset from HuggingFace and add it to ChromaDB for FAQ Agent
+// processInsuranceDatasetToChroma();
+
+// Test ChromaDB using a query
+// const collection = await getInsuranceQACollection();
+// collection.query({
+// 	queryTexts: ["What is the process to file a claim?"],
+// }).then((result) => {
+// 	console.log("ChromaDB Query Result:", result);
+// }).catch((error) => {
+// 	console.error("Error querying ChromaDB:", error);
+// });
