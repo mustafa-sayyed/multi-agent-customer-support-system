459809cfa6f5b022caf02310847cc57b26a1b3e9
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2026-05-23 00:11:35 +0530

    add state schema

---
+++ src/state.ts
@@ -0,0 +1,32 @@
+import { MessagesValue, StateSchema } from "@langchain/langgraph";
+import z, { custom } from "zod";
+
+export const state = new StateSchema({
+	messages: MessagesValue,
+    conversationHistory: z.string().optional(),
+    userInput: z.string().optional(),
+
+    // User related extracted fields
+    userIntent: z.string().optional(),
+    policyNumber: z.string().optional(),
+    claimId: z.string().optional(),
+    customerId: z.string().optional(),
+
+    // Supervisor agent handles these fields
+	nextAgent: z.string().optional(),
+	reasonForNextAgent: z.string().optional(),
+	task: z.string().optional(),
+
+    // Human escalation related fields
+    requireHumanEscalation: z.boolean().optional(),
+    escalationReason: z.string().optional(),
+
+    // Billing related information
+    billingAmount: z.number().optional(),
+    paymentMethod: z.string().optional(),
+    billingFrequency: z.string().optional(),
+    invoiceDate: z.string().optional(),
+
+    timestamp: z.string().optional(),
+    finalAnswer: z.string().optional(),
+});
